48174db1bf270d8ee3bab4daf956404ddf72a0c8
aleksandramikasevic20140040 <amikasevic@gmail.com>
2016-03-23 23:22:25 +0100

    Uvedene metode toString, equals i hashCode.

--- Menjacnica/src/menjacnica/Kurs.java
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -31,6 +31,39 @@ public GregorianCalendar getDatumKursa() {
 public void setDatumKursa(GregorianCalendar datumKursa) {
 	this.datumKursa = datumKursa;
 }
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	long temp;
+	temp = Double.doubleToLongBits(kupovni);
+	result = prime * result + (int) (temp ^ (temp >>> 32));
+	temp = Double.doubleToLongBits(prodajni);
+	result = prime * result + (int) (temp ^ (temp >>> 32));
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Kurs other = (Kurs) obj;
+	if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+		return false;
+	if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+		return false;
+	return true;
+}
+@Override
+public String toString() {
+	return "Kurs [kupovni =" + kupovni + ", srednji =" + srednji + ", prodajni =" + prodajni + ", datumKursa:" + datumKursa.getTime()
+			+ "]";
+}
+
+
 }
 
 
