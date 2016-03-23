3667f8f3330fe59e45b9b2af64af71ccc504e304
aleksandramikasevic20140040 <amikasevic@gmail.com>
2016-03-23 23:43:54 +0100

    Implementirane metode interfejsa, ali su nefunkcionalne.

---
+++ Menjacnica/src/menjacnica/ImplementirajInterfejs.java
@@ -0,0 +1,15 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class ImplementirajInterfejs {
+	public void dodatiKurs(double kupovni, double srednji, double prodajni, GregorianCalendar datum){}
+	public void obrisatiKurs(GregorianCalendar datum){}
+	
+	public Kurs pronadjiKurs(GregorianCalendar datum){
+		Kurs p = new Kurs();
+		return p;
+		
+	}
+
+}
